77edd64f050d148bcf956633eddaa4a86e6d0771
Yauri Attamimi <yauritux@gmail.com>
2021-11-01 17:56:14 +0700

    bootstraping grpc server

--- src/main/java/com/pdd/yo2/brms/GrpcServer.java
+++ src/main/java/com/pdd/yo2/brms/GrpcServer.java
@@ -1,2 +1,27 @@
-package com.pdd.yo2.brms;public class GrpcServer {
+package com.pdd.yo2.brms;
+
+import com.pdd.yo2.brms.infrastructure.input.api.grpc.RemittanceGrpcServiceImpl;
+import com.pdd.yo2.brms.infrastructure.output.dao.drools.RemittanceRuleMediatorRepositoryImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GrpcServer {
+
+    public static void main(String[] args) {
+        var remittanceRuleRepository = new RemittanceRuleMediatorRepositoryImpl();
+        var remittanceService = new RemittanceGrpcServiceImpl(remittanceRuleRepository);
+        Server server = ServerBuilder
+                .forPort(50055)
+                .addService(remittanceService).build();
+        try {
+            server.start();
+            server.awaitTermination();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
